a71160853a9869c58093acaae605f17335815f70
sridev <69075577+sridevkp@users.noreply.github.com>
2025-08-31 16:03:20 +0530

    in memory

--- src/chat.py
+++ src/chat.py
@@ -1,24 +1,24 @@
-
-from ingest import get_vectorstore
 from langchain.chains import RetrievalQA
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv() 
 
-retriever = get_vectorstore().as_retriever(search_kwargs={"k": 4})
 
-qa = RetrievalQA.from_chain_type(
-    llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0),
-    retriever=retriever,
-    chain_type="map_reduce" ,
-    return_source_documents=True
-)
+def get_llm(index):
+    retriever = index.vectorstore.as_retriever(search_kwargs={"k": 4})
 
-def llm(query):
-    response = qa.invoke({"query": query})
-    return response["result"]
-
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0),
+        retriever=retriever,
+        chain_type="map_reduce" ,
+        return_source_documents=True
+    )
+    def llm(query):
+        response = qa.invoke({"query": query})
+        return response["result"]
+    
+    return llm
 
 if __name__ == "__main__":
     print("This is a package file!")
